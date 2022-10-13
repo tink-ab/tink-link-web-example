54333ee9598a0176aba1cc9aceb08a90af760178
Gustav Fredriksson <gustav.fredriksson@tink.com>
2022-10-13 16:58:42 +0200

    refactor: make url construction prettier

--- client/src/components/AuthorizationLink.jsx
+++ client/src/components/AuthorizationLink.jsx
@@ -2,18 +2,12 @@ import React from "react";
 import { Button } from "reactstrap";
 
 export const AuthorizationLink = ({ locale, market, scope }) => {
-  const link =
-    "https://link.tink.com/1.0/authorize/?" +
-    "client_id=" +
-    process.env.REACT_APP_CLIENT_ID +
-    "&redirect_uri=http://localhost:3000/callback" +
-    "&scope=" +
-    scope +
-    ssnData +
-    "&market=" +
-    market +
-    "&locale=" +
-    locale;
+  const url = new URL("https://link.tink.com/1.0/authorize/?");
+  url.searchParams.set("client_id", process.env.REACT_APP_CLIENT_ID);
+  url.searchParams.set("redirect_uri", "http://localhost:3000/callback");
+  url.searchParams.set("scope", scope);
+  url.searchParams.set("market", market);
+  url.searchParams.set("locale", locale);
 
-  return <Button href={link}>Connect Bank</Button>;
+  return <Button href={url.toString()}>Connect Bank</Button>;
 };
